38b642c68998b655c94392c9d31241f51f80cfe8
gregoryawesoman <ray_couper@mail.ru>
2020-03-17 22:01:49 +0300

    added main template

--- main.rb
+++ main.rb
@@ -1,3 +1,59 @@
+Dir['./lib/*.rb'].sort.each { |file| require file }
+
 class Main
+
+  ACTIONS = <<-LIST
+    1 - Pass
+    2 - Add card
+    3 - Show cards
+  LIST
+
+  def initialize
+    @player = HumanPlayer.new('Name')
+    @diller = ComputerPlayer.new
+
+    @players = [player, diller]
+    @current_player = player
+    puts 'Welcome to Casino Royale!'
+    @card_deck = []
+  end
+
+  def start_game
+    loop do
+      puts 'Make your turn', ACTIONS
+      input = gets.chomp.to_i
+      case input
+      when 1 then pass
+      when 2 then add_card
+      when 3 then show_cards
+      end
+    end
+  end
+
+  private
+
+  def prepare_deck
+    
+  end
+
+  def pass
+    current_player.pass
+    change_player
+  end
+
+  def change_player
+
+  end
+
+  def add_card
+    current_player.add_card
+    change_player
+  end
+
+  def show_cards
+
+  end
+
+
   
 end
