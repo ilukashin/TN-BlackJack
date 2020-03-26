c9656d43e0503e4a0e46668a19187e5a63f6cdc8
gregoryawesoman <ray_couper@mail.ru>
2020-03-26 09:16:17 +0300

    card object fix

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Card
+  CARDS = %w[A 2 3 4 5 6 7 8 9 10 J Q K].freeze
+  POINTS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10].freeze
+  SUITES = %w[♠ ♡ ♣ ♢].freeze
+
   attr_accessor :value
   attr_reader :name
 
--- lib/deck.rb
+++ lib/deck.rb
@@ -3,19 +3,15 @@
 class Deck
   attr_accessor :cards
 
-  CARDS = %w[A 2 3 4 5 6 7 8 9 10 J Q K].freeze
-  POINTS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10].freeze
-  SUITES = %w[♠ ♡ ♣ ♢].freeze
-
   def initialize
     new_deck
   end
 
   def new_deck
     @cards = []
-    SUITES.each do |suite|
-      CARDS.each do |card|
-        cards << Card.new("#{card}#{suite}", POINTS[CARDS.index(card)])
+    Card::SUITES.each do |suite|
+      Card::CARDS.each_with_index do |card, i|
+        cards << Card.new("#{card}#{suite}", Card::POINTS[i])
       end
     end
   end
